fe7ef1da00e326d0c2d258e67715b9e26bbb90bc
unknown <Sanket.Shah@ipad84042.synechron.com>
2016-02-09 17:05:20 +0530

    Done

--- com/Sanket/module/app.js
+++ com/Sanket/module/app.js
@@ -1,9 +1,9 @@
-( function() {
+( function() {		
 		angular.module("app", ["ngRoute"]).config(function($routeProvider) {
 			$routeProvider.when("/hero", {
 				controller : "herocontroller",
 				templateUrl : "com/Sanket/views/hero.html"
-			}).when("/movies", {
+			}).when("/movies/:hid", {
 				controller : "moviecontroller",
 				templateUrl : "com/Sanket/views/movies.html"
 			});
